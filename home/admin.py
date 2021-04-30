d278dc2efc7eba65c742140a23b7ea5d328dd94a
Mr-Potter-Hack <61103201+Mr-Potter-Hack@users.noreply.github.com>
2021-04-30 14:11:01 +0530

    Delete home directory

--- home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from home.models import Contact
-from .models import Post
-
-# Register your models here.
-
-admin.site.register(Contact)
-admin.site.register(Post)
